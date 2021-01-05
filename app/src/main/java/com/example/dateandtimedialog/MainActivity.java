7efcabcce7111f17eeb9db7641ac20d6796083a0
miaobaobao <joychoinjapan@gmail.com>
2021-01-06 00:29:55 +0900

    add datepicker

--- app/src/main/java/com/example/dateandtimedialog/MainActivity.java
+++ app/src/main/java/com/example/dateandtimedialog/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.dateandtimedialog;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.ArraySet;
 import android.util.Log;
@@ -14,11 +17,16 @@ import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.crypto.AEADBadTagException;
@@ -26,6 +34,9 @@ import javax.crypto.AEADBadTagException;
 
 public class MainActivity extends AppCompatActivity {
     private TextView resultTextView;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private DatePicker.OnDateChangedListener mDateChangedListener;
+    SimpleDateFormat simpleDateFormat =new SimpleDateFormat("EE, MMMM dd, yyyy",Locale.US);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     public void myClick(View view){
         switch (view.getId()){
             case R.id.date:
+                pickTime();
                 break;
             case R.id.option:
                 showOptionDialog(view);
@@ -47,6 +59,59 @@ public class MainActivity extends AppCompatActivity {
                 showCheckBoxDialog(view);
                 break;
         }
+    }
+
+    private void pickTime() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int day_week = cal.get(Calendar.DAY_OF_WEEK);
+
+        Date date = new Date();
+        //SimpleDateFormat simpleDateFormat =new SimpleDateFormat("EE, MMMM dd, yyyy",Locale.US);
+        String show_date = simpleDateFormat.format(date);
+        DatePickerDialog dialog = new DatePickerDialog(this,
+                android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener,
+                year, month, day);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setTitle(show_date);
+
+        dialog.getDatePicker().init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Log.i("TAG", "ischanged");
+                Date date_changed = new Date(year - 1900, monthOfYear, dayOfMonth);
+                Log.i("TAG", "" + date_changed);
+                String show_date = simpleDateFormat.format(date_changed);
+                dialog.setTitle(show_date);
+            }
+        });
+        dialog.show();
+
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                Toast.makeText(MainActivity.this, year + "年" + month + "月" + dayOfMonth + "日", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+
+        };
+
+
+//        mDateChangedListener = new DatePicker.OnDateChangedListener() {
+//            @Override
+//            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+//                Log.i("TAG", "ischanged");
+//                Date date_changed = new Date(year - 1900, monthOfYear, dayOfMonth);
+//                Log.i("TAG", "" + date_changed);
+//                String show_date = simpleDateFormat.format(date_changed);
+//                dialog.setTitle(show_date);
+//            }
+//
+//        };
     }
 
 
@@ -69,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(),"!!你选择了："+items[position],Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"你选择了："+items[position],Toast.LENGTH_SHORT).show();
                 alertDialog.dismiss();
             }
         });
